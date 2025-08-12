848c7091d4b59bedcd4b47c6a1a86829b9dabdb9
gimura <gimura0001@gmail.com>
2025-08-12 22:18:59 +0300

    changes

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,12 +1,21 @@
 { pkgs, config, nixpkgs-stable, ... }: let
 	stablePkgs = import nixpkgs-stable { system = pkgs.system; };
 in {
+	nixpkgs.config.packageOverrides = pkgs: {
+		neocd = pkgs.callPackage /home/gimura/projects/neocd/default.nix;
+	};
+
 	environment.systemPackages = with pkgs; [
 		git
 		gcc
 		unzip
 		wget
 		gnumake
+		gdb
+		clisp
+		mandoc
+
+		~/projects/neocd/default.nix
 
 		hyprland
 		xdg-desktop-portal-hyprland
