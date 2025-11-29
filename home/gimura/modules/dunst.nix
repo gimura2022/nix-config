b7772b595e6f16873013bedab78e5734e9e50f9d
gimura <gimura0001@gmail.com>
2025-11-29 03:11:37 +0300

    configured dunst

--- home/gimura/modules/dunst.nix
+++ home/gimura/modules/dunst.nix
@@ -1,5 +1,22 @@
 {
   services.dunst = {
     enable = true;
+
+    settings = {
+      global = {
+        width = "(300,320)";
+        height = "(120,140)";
+        offset = "(20,20)";
+        origin = "top-right";
+        frame_color = "#bac2de";
+        font = "CaskaydiaCove Nerd Font";
+      };
+
+      urgency_normal = {
+        background = "#1e1e2e";
+        foreground = "#cdd6f4";
+        timeout = 10;
+      };
+    };
   };
 }
